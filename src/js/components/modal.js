585e5dd07207989d2544c66c489ba134cddfbb01
1ike <googlohren@gmail.com>
2019-06-30 01:09:37 +0300

    Refine styles for modal.

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -6,12 +6,16 @@ const tarifElem = document.getElementsByClassName('modal__body')[0];
 export default (props) => {
   const { modal: { isOpen, tarif } } = props;
   const modalOpenClassName = 'modal--open';
+  const bodyModalOpenClassName = 'modal-open';
 
   if (!isOpen) {
     modalElem.classList.remove(modalOpenClassName);
+    document.body.classList.remove(bodyModalOpenClassName);
     return;
   }
+
   modalElem.classList.add(modalOpenClassName);
+  document.body.classList.add(bodyModalOpenClassName);
 
   const tarifCard = renderTarifCard({ tarif });
   tarifElem.innerHTML = '';
